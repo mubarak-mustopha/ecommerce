ecd3649b38b930c9a94e7430d9740764c2a71a26
mubaarock <mustophamubarack17@gmail.com>
2024-10-15 03:46:00 +0100

    Add password reset functionality

--- zayshop/settings.py
+++ zayshop/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # local
     "accounts.apps.AccountsConfig",
+    "products.apps.ProductsConfig",
 ]
 
 MIDDLEWARE = [
--- zayshop/urls.py
+++ zayshop/urls.py
@@ -20,7 +20,8 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("",TemplateView.as_view(template_name="index.html"), name = "home"),
+    path("", TemplateView.as_view(template_name="index.html"), name="home"),
     path("accounts/", include("accounts.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("admin/", admin.site.urls),
 ]
